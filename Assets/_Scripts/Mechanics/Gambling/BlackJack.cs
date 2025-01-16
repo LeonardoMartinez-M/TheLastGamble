60c30af63853ec0a674860a2eebe8bd6f157c6be
Reyes <reyesjsal@gmail.com>
2025-01-16 14:09:50 -0700

    More Script For BlackJack
    :3 worked on the randomization and the storing of th e card values for you can find them to later change the shown cards on the Gun and UI

--- Assets/_Scripts/Mechanics/Gambling/BlackJack.cs
+++ Assets/_Scripts/Mechanics/Gambling/BlackJack.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class BlackJack : MonoBehaviour
 {
+    public bool Active;
     //creates the cards and their values for visual finding and the value for the buffs
     public string[12] cardsNum;
     public int[12] cardsVal;
+   
     //The Hand of the player
-    private int Hand = new int Hand; 
+    public int Hand;
+    public int HandNums[0][0];
+
     // creates the value for the Ace
     public int findValue(int hand)
     {
@@ -20,7 +24,10 @@ public class BlackJack : MonoBehaviour
         {
             return 1;
         }
+        cout<<"Find Values Called">>
     }
+
+    //makes the array's populate with the cards
     public void MakeCards(int[] Value, string[] Name)
     {
         //makes the cards traceable to the visual assets
@@ -51,20 +58,56 @@ public class BlackJack : MonoBehaviour
           Value[10] = 10;
           Value[11] = 10;
           Value[12] = 10;
+          cout<<"Make Cards Called">>
           return this;
 
     }
-    
-    
+
+    //Makes the hand
+    public int MakeHand(HandNums cards, Hand total)
+    {
+        cout<<"Make Hand Called">>
+        return cards.DrawHand(cardsVal[],cardsVal[], Hand), total;
+    }
+
+    private int DrawHand(int[] CardOne, int[] CardTwo, int total)
+    {
+        cout<<"DrawHand called">>
+        int random = 0 + (rand() % 12);
+        int random2 = 0 + (rand() % 12);
+        int total =  CardOne[random].position+CardTwo[random2].position
+        
+        return CardOne[random].position,CardTwo[random2].position, total;
+    }
+
+    private ApplyEffect(int HandTotal)
+    {
+        cout<<"Apply Effect Called">>
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
+        Active = false;
         MakeCards(cardsVal[], cardsNum[]);
+        cout<<"Start Called">>
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Active == true)
+        { 
+            MakeHand(HandNums,Hand);
+            ApplyEffect(Hand);
+            ChangeVisuals(HandNums[0][0]);
+            Active = false;
+        }
+        else
+        {
+            return null;
+        }
+        cout<<"Update Called">>
     }
 }
