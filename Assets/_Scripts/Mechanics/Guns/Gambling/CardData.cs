2ab99cde8293643e52885a71b68aaeb425967f0f
Reyes <reyesjsal@gmail.com>
2025-04-30 14:31:49 -0600

    :3

--- Assets/_Scripts/Mechanics/Guns/Gambling/CardData.cs
+++ Assets/_Scripts/Mechanics/Guns/Gambling/CardData.cs
@@ -29,6 +29,23 @@ public class CardData
         King,
         Ace
     }
+    public enum CardsBak
+    {
+        Ace = 1,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King = 13
+    }
     public enum FaceBj
     {
         Two = 2,
@@ -47,10 +64,10 @@ public class CardData
     }
   
 
-    private Face _face;
-    private FaceBj _faceBj;
-    private Suit _suit;
-
+    public Face _face;
+    public FaceBj _faceBj;
+    public Suit _suit;
+    public CardsBak _value;
     // Constructor to randomly generate a card
     public CardData()
     {
@@ -60,7 +77,8 @@ public class CardData
         _face = (Face)rand.Next(2, 15);  // Faces: 2-14 (Ace is 14)
         _suit = (Suit)rand.Next(1, 5);   // Suits: 1-4 (Clubs, Diamond, Hearts, Spades)
         _faceBj = (FaceBj)rand.Next(2, 12);  // Faces: 2-14 (Ace is 14)
-       
+        _value = (CardsBak)rand.Next(1, 13);// Value: 1-13 (10+ will be valued at 0 in the script later on :3)
+
     }
 
     // Constructor to create a card with specified face and suit
