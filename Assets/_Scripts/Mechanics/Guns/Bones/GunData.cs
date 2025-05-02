7d8c74abe75fc8200ea0d0c7c111c85145e4bc67
Reyes <reyesjsal@gmail.com>
2025-05-02 13:19:14 -0600

    e3

--- Assets/_Scripts/Mechanics/Guns/Bones/GunData.cs
+++ Assets/_Scripts/Mechanics/Guns/Bones/GunData.cs
@@ -2,10 +2,9 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-
 [CreateAssetMenu(fileName = "Gun Info", menuName = "Stats/GunStats")]
 public class GunData : ScriptableObject
-{ 
+{
     [Header("Frame")]
     private MonoScript controller;
     public UnityEngine.Object prefab;
@@ -18,17 +17,23 @@ public class GunData : ScriptableObject
     public bool canPen = false;
     public bool canBurn = false;
     public bool hasShrapnel = false;
-    
+
     [Header("Combat Stats")]
     //chance of an effect happening
     public int chance = 0;
     // damage
-    public long damage;
+    [SerializeField] private long _damage;
+    public long damage
+    {
+        get => _damage;
+        set => _damage = value;
+    }
     public long totalDamage = 390;
     // fire rate
     public float fireRate = 850f;
     // mag size
     public long magSize = 39;
+   
     // default spare ammo
     public float spareAmmo = 210;
     // ads speed
@@ -43,15 +48,22 @@ public class GunData : ScriptableObject
     public int stunDuration = 0;
     public int impairSeverity = 0;
     public int impairDuration = 0;
+
     public GunData()
-        {
-            // Initialize magSize here, or it might be assigned elsewhere
-            magSize = 39; // Example value
-            // Calculate adjusted and assign to damage in the constructor
-            long adjusted = totalDamage / magSize;
-            damage = adjusted;
-        }
-    
+    {
+        UpdateDamage(); // Calculate initial damage
+    }
+
+    private void OnValidate()
+    {
+        UpdateDamage(); // Recalculate damage in the editor when values change
+    }
+
+    private void UpdateDamage()
+    {
+        damage = (long)(totalDamage / Mathf.Max(1, magSize));
+    }
+
     [Header("Recoil Stats")]
     // bloom
     public float hipBloom;
@@ -67,5 +79,4 @@ public class GunData : ScriptableObject
     public float swayIntensity;
     // smoothing
     public float smoothing;
-    
 }
